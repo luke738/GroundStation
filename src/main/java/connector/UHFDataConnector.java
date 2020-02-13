a89e318a5079285e069e4ac9d1355c0000936cec
Luke St. Regis <stregis@usc.edu>
2020-02-12 18:03:52 -0800

    Get UHF data sending connected

--- src/main/java/connector/UHFDataConnector.java
+++ src/main/java/connector/UHFDataConnector.java
@@ -1,8 +1,12 @@
 package connector;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import info.Connection;
+import info.Message;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,25 +14,45 @@ public class UHFDataConnector implements DataListener{
     private List<DataListener> listeners = new ArrayList<DataListener>();
     Connection desktopConn;
 
-    private static UHFDataConnector single = null;
 
+    private static UHFDataConnector single = null;
     private UHFDataConnector() {
-        //initialize();
+        //initialize(); TODO: Uncomment when initialize works
     }
 
     private void initialize() {
-        //Connect to desktop program
+        //TODO: Connect to desktop program
         //Initialize desktopConn
         desktopConn.addDataListener(this);
     }
 
-    public UHFDataConnector getInstance(){
+    public static UHFDataConnector getInstance(){
         if(single==null) single = new UHFDataConnector();
 
         return single;
     }
 
-    private Boolean validate(JsonElement data) {
+    public void setTransmitState(Boolean state) {
+        //TODO: Construct correct json message
+        JsonElement message = new JsonObject();
+        desktopConn.send(message);
+    }
+
+    public Boolean sendData(JsonElement data) {
+        if(validateSend(data))
+        {
+            desktopConn.send(data);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    private Boolean validateRecieve(JsonElement data) {
+        return true;
+    }
+
+    private Boolean validateSend(JsonElement data) {
         return true;
     }
 
@@ -41,15 +65,33 @@ public class UHFDataConnector implements DataListener{
     }
 
     private void sendEvent(JsonElement data) {
-        if(validate(data))
+        List<DataListener> dead = new ArrayList<>();
+        if(validateRecieve(data))
         {
             for (DataListener d : listeners)
             {
+                if(d==null) {
+                    dead.add(d);
+                    continue;
+                }
                 d.dataReceived(data);
+            }
+            for (DataListener d : dead) {
+                listeners.remove(d);
             }
         }
         else {
-
+            for (DataListener d : listeners)
+            {
+                if(d==null) {
+                    dead.add(d);
+                    continue;
+                }
+                d.dataReceived(new Gson().toJsonTree(new Message("invalid")));
+            }
+            for (DataListener d : dead) {
+                listeners.remove(d);
+            }
         }
     }
 }
--- src/main/java/socket/UHFDataSocket.java
+++ src/main/java/socket/UHFDataSocket.java
@@ -1,7 +1,11 @@
 package socket;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import connector.DataListener;
+import connector.UHFDataConnector;
+import info.Message;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
@@ -16,23 +20,45 @@ import java.util.List;
 public class UHFDataSocket implements DataListener
 {
     private static List<Session> clients = Collections.synchronizedList(new ArrayList<Session>());
+    private UHFDataConnector connector = UHFDataConnector.getInstance();
+    private JsonParser parser = new JsonParser();
+    private Gson gson = new Gson();
+
+    public UHFDataSocket()
+    {
+        connector.addDataListener(this);
+    }
 
     @OnOpen
     public void open(Session session, EndpointConfig endpoint)
     {
-
+        clients.add(session);
     }
 
     @OnMessage
     public void onMessage(String message, Session session)
     {
-
+        JsonElement data = parser.parse(message);
+        //TODO: check session to see if user has edit access
+        if(data.getAsJsonObject().get("type").getAsString().equalsIgnoreCase("transmitState")) {
+            connector.setTransmitState(data.getAsJsonObject().getAsString().equalsIgnoreCase("true"));
+        }
+        else {
+            Boolean success = connector.sendData(data);
+            try
+            {
+                session.getBasicRemote().sendText(gson.toJson(new Message(success ? "success" : "failure")));
+            }
+            catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
     }
 
     @OnClose
     public void close(Session session)
     {
-
+        clients.remove(session);
     }
 
     @OnError
