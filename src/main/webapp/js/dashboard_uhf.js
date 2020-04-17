8d57085877ff2b12d759c82fa4155bd3f21a2b05
sophiahu12 <husophia@usc.edu>
2020-04-17 00:46:02 -0700

    adding program ctrl btns on uhf dashboard

--- src/main/webapp/js/dashboard_uhf.js
+++ src/main/webapp/js/dashboard_uhf.js
@@ -4,8 +4,14 @@ var slider = document.getElementById("antAngleSlider");
 var output = document.getElementById("set_angle");
 output.innerHTML = slider.value;
 
+//program control: show kill desktop button if admin
+var admin = sessionStorage.getItem("isAdmin");
+if (admin == "true") {
+    document.getElementById("shutdown").style.display = "block";
+}
+
 socket.addEventListener('open', function(event) {
-    socket.send(JSON.stringify({header: "hello uhf data socket", body: ""}));
+    socket.send(JSON.stringify({}));
 });
 
 socket.addEventListener('message', function(event) {
@@ -77,4 +83,41 @@ function download1() {
         document.querySelector("#download_onsuccess").innerHTML = "TLE file downloaded";
     }
 
+}
+
+function program_sendData(clicked_id) {
+
+    var programAction = clicked_id; //launch_orbitron, kill_orbitron, or shutdown
+    var data = null;
+    if (programAction == "shutdown") {
+        data = JSON.stringify({header: "shutdown"});
+    } else {
+        var programActionArr = programAction.split("_");
+
+        var action = programActionArr[0]; //launch or kill
+        if (action === "launch") {
+            action = "start_program";
+        } else if (action === "kill") {
+            action = "stop_program"
+        }
+        var program = programActionArr[1]; //which program
+
+        data = JSON.stringify({ header: action, body: program });
+    }
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "/ProgramControl", false);
+    xhr.send(data);
+
+    console.log("RESP: " + xhr.response);
+
+    var response = JSON.parse(xhr.response); //Could check and see if request was successful
+
+    var responseArray = response.header.split("_");
+    if (responseArray[0] === "shutdown") {
+        document.querySelector('#shutdown_onsuccess').innerHTML = " " + responseArray[0] + " " + responseArray[1];
+    } else {
+        document.querySelector('#onsuccess').innerHTML = responseArray[0] + " " + program + " " + responseArray[1];
+    }
 }
