a2e8c45da0ea5149d9344d4feb62a704aeae467f
sophiahu12 <husophia@usc.edu>
2020-05-04 00:20:20 -0700

    frontend clears session log out

--- src/main/webapp/js/logout.js
+++ src/main/webapp/js/logout.js
@@ -2,13 +2,14 @@ function logout() {
     sessionStorage.clear();
 
     //go to login servlet and say user logged out
-    var data = {};
+    var data = JSON.stringify({header: "logout"});
+
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/Login", false);
     xhr.send(data);
 
-    console.log("RESP: " + xhr.response);
+    //console.log("RESP: " + xhr.response);
+    //var response = JSON.parse(xhr.response); //Could check and see if request was successful
 
-    var response = JSON.parse(xhr.response); //Could check and see if request was successful
 
 }
